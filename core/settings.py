f0258082a0cf9cc43e8875f318bbe3a2c60b8dd6
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-10-25 11:42:14 +0300

    after adding djangorestframework-simplejwt

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     
     # third party packages
     "rest_framework",
+    'rest_framework_simplejwt',
     "corsheaders",
     
     # local apps
@@ -64,6 +66,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5), # default
+}
 
 CORS_ALLOWED_ORIGINS = (
         "http://localhost:3000",
