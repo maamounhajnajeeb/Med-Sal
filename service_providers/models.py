fad16e4097e3b5442b65704f899ca0e9a0cadbbb
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-10-30 19:26:24 +0200

    migrations round 2

--- service_providers/models.py
+++ service_providers/models.py
@@ -6,7 +6,7 @@ from users.models import Users, Admins
 
 class ServiceProvider(models.Model):
     
-    class AccountStatus(models.TextChoices):
+    class Status(models.TextChoices):
         REJECTED = ('rejected', 'Rejected')
         PENDING = ('pending', 'Pending')
         ACCEPTED = ('accepted', 'Accepted')
@@ -20,32 +20,9 @@ class ServiceProvider(models.Model):
     iban = models.CharField(max_length=40, null=False, unique=True)
     swift_code = models.CharField(max_length=16, null=False, unique=True)
     provider_file = models.FileField(upload_to="service_providers/documents/", null=False)
-    account_status = models.CharField(max_length=16,
-                choices=AccountStatus.choices, default=AccountStatus.PENDING)
+    account_status = models.CharField(max_length=16, choices=Status.choices, default=Status.PENDING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
-    # user = models.OneToOneField(Users, on_delete = models.CASCADE, related_name = 'service_provider')
-    # approved_by = models.ForeignKey(Admins, on_delete = models.SET_NULL, null = True, blank = True, related_name='approved_service_providers')
-    # category = models.ForeignKey(Category, blank=True, null=True, on_delete = models.CASCADE)
-    
-    # business_name = models.CharField(max_length = 128, null = True) 
-    
-    # contact_number = models.CharField(max_length = 32, null = True)
-    
-    # bank_name = models.CharField(max_length = 128, null = True)
-    
-    # iban = models.CharField(max_length = 32, null = True)
-    
-    # swift_code = models.CharField(max_length = 32, null = True)
-    
-    # provider_file = models.FileField(upload_to = "service_providers/documents/", null = True)
-    
-    # account_status = models.CharField(max_length = 20, choices = AccountStatus.choices, default = AccountStatus.PENDING)
-    
-    # created_at = models.DateTimeField(auto_now_add = True)
-    
-    # updated_at = models.DateTimeField(auto_now = True)
     
     base_type = Users.Types.SERVICE_PROVIDER
     
@@ -53,25 +30,18 @@ class ServiceProvider(models.Model):
         return str(self.user)
     
     class Meta:
-        # ordering = ['date_joined']
         verbose_name = "ServiceProvider"
         verbose_name_plural = "ServiceProviders"
 
 
 class ServiceProviderLocations(models.Model):
-    
     service_provider_id = models.ForeignKey(ServiceProvider, on_delete = models.CASCADE, null = True)
-
     location = models.CharField(max_length=20, blank=False)
-
     opening = models.TimeField(blank=False)
-    
     closing = models.TimeField(blank=False)
-
     crew = models.CharField(max_length = 32, blank=False)
-
     created_at = models.DateTimeField(auto_now_add = True, null = False)
-
+    
     def __str__(self):
         return self.service_provider_id.business_name
     
