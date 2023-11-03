f7fcf13ae42b4172dde9041212126e56e6bab4f7
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-11-03 10:14:47 +0200

    after changing between JSONField and CharField

--- users/views.py
+++ users/views.py
@@ -24,12 +24,11 @@ class CustomUserViewSet(UserViewSet):
     permission_classes = (AllowAny, )
     http_method_names = ["get", "post", "patch", "put", "delete"]
     
-    def create(self, request):
+    def create(self, request, *args, **kwargs):
         user_data = request.data
-        user_type = user_data.get("user_type")
         
-        response = super().create(request) # default register
-        if user_type != Users.Types.SERVICE_PROVIDER:
+        response = super().create(request, *args, **kwargs)# default register
+        if response.data["user_type"] != Users.Types.SERVICE_PROVIDER:
             return Response(response.data, status=response.status_code)
         
         # else continue and regiter the Service Provider
@@ -38,17 +37,17 @@ class CustomUserViewSet(UserViewSet):
         
         # Fetch the Category instance based on the user_data value
         category_value = user_data.get("category")
-        category_instance = get_object_or_404(Category, name=category_value)
+        category_instance = get_object_or_404(Category, id=category_value)
         
         ServiceProvider.objects.create(
-            user_id=super_user_id,
-            business_name=user_data.get("business_name"),
-            contact_number=user_data.get("contact_number"),
-            bank_name=user_data.get("bank_name"),
-            category=category_instance,
-            iban=user_data.get("iban"),
-            swift_code=user_data.get("swift_code"),
-        )
+            user_id=super_user_id
+            , business_name=user_data.get("business_name")
+            , contact_number=user_data.get("contact_number")
+            , bank_name=user_data.get("bank_name")
+            , category=category_instance
+            , iban=user_data.get("iban")
+            , swift_code=user_data.get("swift_code")
+            , )
         
         return Response(super_user, status=response.status_code)
 
