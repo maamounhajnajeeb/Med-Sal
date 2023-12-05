98052628f8b509dc1cdf33db20471ab5bd459d45
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-12-05 20:27:03 +0200

    after testing category

--- category/views.py
+++ category/views.py
@@ -3,7 +3,6 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 
 from django.http import HttpRequest
-from django.db.models import Q
 
 from .serializers import CategorySerializer
 from .helpers import searching_func
@@ -12,13 +11,14 @@ from .models import Category
 
 from service_providers.serializers import LocationSerializerSafe
 from service_providers.models import ServiceProviderLocations
+from notification.models import Notification
 
 
 
 @decorators.api_view(["GET", ])
 @decorators.permission_classes([permissions.AllowAny, ])
-def parent_sub_category(request, pk):
-    third_field = request.META.get("HTTP_ACCEPT-LANGUAGE")
+def parent_sub_category(request, pk: int):
+    third_field = request.META.get("Accept-Language")
     
     queryset = Category.objects.filter(parent=pk)
     serializer = CategorySerializer(queryset, fields={"id", "parent", f"{third_field}_name"}, many=True)
@@ -28,7 +28,7 @@ def parent_sub_category(request, pk):
 @decorators.api_view(["GET", ])
 @decorators.permission_classes([permissions.AllowAny, ])
 def prime_categories(request):
-    third_field = request.META.get("HTTP_ACCEPT-LANGUAGE")
+    third_field = request.META.get("Accept-Language")
 
     queryset = Category.objects.filter(parent=None)
     serializer = CategorySerializer(queryset, fields={"id", "parent", f"{third_field}_name"}, many=True)
@@ -38,7 +38,7 @@ def prime_categories(request):
 @decorators.api_view(["GET", ])
 @decorators.permission_classes([permissions.AllowAny, ])
 def search_category(request: HttpRequest):
-    third_field = request.META.get("HTTP_ACCEPT-LANGUAGE")
+    third_field = request.META.get("Accept-Language")
     queryset = searching_func(request, third_field)
     
     if not queryset.exists():
@@ -78,17 +78,37 @@ class CategoryViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def retrieve(self, request, *args, **kwargs):
-        third_field = request.META.get("HTTP_ACCEPT-LANGUAGE")
+        third_field = request.META.get("Accept-Language")
         
         instance = self.get_object()
         serializer = self.get_serializer(instance, fields={"id", "parent", f"{third_field}_name"})
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request, *args, **kwargs):
+        resp = super().create(request, *args, **kwargs)
+        
+        Notification.objects.create(
+            sender="System", sender_type="System"
+            , receiver=request.user.email, receiver_type="System"
+            , en_content=f"{resp.data['en_name']} category added"
+            , ar_content=f" تمت إضافة {resp.data['ar_name']}")
+        
+        return Response(resp.data, status=resp.status_code)
+    
+    def perform_destroy(self, instance: Category):
+        Notification.objects.create(
+            sender="System", sender_type="System"
+            , receiver=self.request.user.email, receiver_type="System"
+            , en_content=f"{instance.en_name} category deleted"
+            , ar_content=f" تم حذف {instance.ar_name}")
+        
+        return super().perform_destroy(instance)
 
 
 @decorators.api_view(["GET", ])
 @decorators.permission_classes([permissions.AllowAny, ])
 def category_locations_filter(request: HttpRequest, category_id: int):
-    locations = ServiceProviderLocations.objects.filter(Q(service_provider__category=category_id))
+    locations = ServiceProviderLocations.objects.filter(service_provider__category=category_id)
     
     serializer = LocationSerializerSafe(locations, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
