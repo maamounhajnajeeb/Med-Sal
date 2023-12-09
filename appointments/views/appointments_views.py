6f55b24ebb2d63a357879e5a0a9f7dec01c429c5
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-12-09 12:59:56 +0200

    after working with services

--- appointments/views/appointments_views.py
+++ appointments/views/appointments_views.py
@@ -7,6 +7,7 @@ from django.http import HttpRequest
 from typing import Optional
 
 from appointments import models, serializers
+from notification.models import Notification
 
 
 
@@ -14,17 +15,39 @@ class CreateAppointment(generics.CreateAPIView):
     queryset = models.Appointments.objects
     serializer_class = serializers.AppointmentSerializer
     
+    def get_serializer(self, *args, **kwargs):
+        language = self.request.META.get("Accept-Language")
+        kwargs.setdefault("language", language)
+        
+        return super().get_serializer(*args, **kwargs)
+    
     def create(self, request: HttpRequest, *args, **kwargs):
-        language = request.META.get("Accept-Language")
+        # language = request.META.get("Accept-Language")
+        # data = request.data.copy()
+        request.data["user"] = request.user.id
+        resp = super().create(request, *args, **kwargs)
         
-        data = request.data.copy()
-        data["user"] = request.user.id
+        Notification.objects.create(
+            sender="System", sender_type="System"
+            , receiver=request.user.email, receiver_type="User"
+            , ar_content="موعدك على قائمة المراجعة لدى مزود الخدمة"
+            , en_content="Your appointments is waiting to be checked from service provider")
         
-        serializer = self.get_serializer(data=data, language=language)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(resp.data, status=status.HTTP_200_OK)
+    
+    # def get_serializer(self, *args, **kwargs):
+        
+    #     serializer_class = self.get_serializer_class()
+    #     kwargs.setdefault('context', self.get_serializer_context())
+    #     return serializer_class(*args, **kwargs)
+    
+    # def create(self, request: HttpRequest, *args, **kwargs):
+        
+    #     serializer = self.get_serializer(data=data, language=language)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class AppointmentRUD(generics.RetrieveUpdateDestroyAPIView):
@@ -35,60 +58,87 @@ class AppointmentRUD(generics.RetrieveUpdateDestroyAPIView):
         language = self.request.META.get("Accept-Language")
         kwargs.setdefault("language", language)
         
-        serializer_class = self.get_serializer_class()
-        kwargs.setdefault('context', self.get_serializer_context())
-        return serializer_class(*args, **kwargs)
+        return super().get_serializer(*args, **kwargs)
+        
+        # serializer_class = self.get_serializer_class()
+        # kwargs.setdefault('context', self.get_serializer_context())
+        # return serializer_class(*args, **kwargs)
     
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer([instance, ], many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        if request.data.get("status"):
+            ar_word = "مقبول" if request.data.get("status") == "accepted" else "مرفوض"
+            Notification.objects.create(
+                sender_type="Service_Provider", sender=request.user.email, receiver_type="User"
+                , reveiver=models.Appointments.objects.get(id=self.kwargs.get("id")).user.email
+                , ar_content=f"موعدك {ar_word}"
+                , en_content=f"Your Appointment has been {request.data.get('status')}")
+        
+        return super().update(request, *args, **kwargs)
 
 
 @decorators.api_view(["GET", ])
-def location_appointments(request: HttpRequest, location_id: int):
+def accepted_location_appointments(request: HttpRequest, location_id: int):
+    """
+    get all accepted appointments for specific provider location
+    """
     language = request.META.get("Accept-Language")
-    queryset = models.Appointments.objects.filter(
-        service__provider_location=location_id, status="accepted")
+    queryset = models.Appointments.objects.filter(service__provider_location=location_id, status="accepted")
     serializer = serializers.ShowAppointmentsSerializer(queryset, many=True, language=language)
     
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @decorators.api_view(["GET", ])
-def all_location_appointments(request: HttpRequest, user_id: int):
-    language = request.META.get("Accept-Language")
-    user_id = user_id or request.user.id
-    
-    queryset = models.Appointments.objects.filter(user__id=user_id, status="accepted")
-    serializer = serializers.ShowAppointmentsSerializer(queryset, many=True, language=language)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@decorators.api_view(["GET", ])
-def provider_appointments(request: HttpRequest, provider_id: Optional[int]):
+def accepted_provider_appointments(request: HttpRequest, provider_id: Optional[int]):
+    """
+    get all accepted appointments for specific provider
+    """
     language = request.META.get("Accept-Language")
     provider_id = provider_id or request.user.id
     queryset = models.Appointments.objects.filter(
-        service__provider_location__service_provider=provider_id
-        , status="accepted")
+        service__provider_location__service_provider=provider_id, status="accepted")
     serializer = serializers.ShowAppointmentsSerializer(queryset, many=True, language=language)
     
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @decorators.api_view(["GET", ])
-def all_provider_appointments(request: HttpRequest, user_id: Optional[int]):
+def all_location_appointments(request: HttpRequest, location_id: int):
+    """
+    get all appointments for specific provider location
+    """
+    language = request.META.get("Accept-Language")
+    queryset = models.Appointments.objects.filter(service__provider_location=location_id)
+    serializer = serializers.ShowAppointmentsSerializer(queryset, many=True, language=language)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@decorators.api_view(["GET", ])
+def all_provider_appointments(request: HttpRequest, provider_id: Optional[int]):
+    """
+    get all appointments for specific provider
+    """
     language = request.META.get("Accept-Language")
     user_id = user_id or request.user.id
-    
-    queryset = models.Appointments.objects.filter(user__id=user_id, status="accepted")
+    queryset = models.Appointments.objects.filter(
+        service__provider_location__service_provider=provider_id)
     serializer = serializers.ShowAppointmentsSerializer(queryset, many=True, language=language)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @decorators.api_view(["GET", ])
-def user_appointments(request: HttpRequest, user_id: Optional[int]):
+def accepted_user_appointments(request: HttpRequest, user_id: Optional[int]):
+    """
+    get all user accepted appointments
+    """
     language = request.META.get("Accept-Language")
     user_id = user_id or request.user.id
     
@@ -99,9 +149,12 @@ def user_appointments(request: HttpRequest, user_id: Optional[int]):
 
 @decorators.api_view(["GET", ])
 def all_user_appointments(request: HttpRequest, user_id: Optional[int]):
+    """
+    get all user appointments 
+    """
     language = request.META.get("Accept-Language")
     user_id = user_id or request.user.id
     
-    queryset = models.Appointments.objects.filter(user__id=user_id, status="accepted")
+    queryset = models.Appointments.objects.filter(user__id=user_id)
     serializer = serializers.ShowAppointmentsSerializer(queryset, many=True, language=language)
     return Response(serializer.data, status=status.HTTP_200_OK)
