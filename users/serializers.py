92d9515fb4a113b49a94055850da4e838995a147
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-12-11 11:47:46 +0200

    after checking users permissions

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers, validators
 
-from django.db import connection
-from django.contrib.auth.models import Group
-from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.db import connection
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
