a788b6a0a31843ba54d6408965d6e9f43cde5c57
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-12-13 13:14:12 +0200

    after adding langauge switcher

--- core/views.py
+++ core/views.py
@@ -4,13 +4,8 @@ from rest_framework.response import Response
 from django.http import HttpRequest
 
 
-@decorators.api_view(["POST", ])
-def change_language(request: HttpRequest):
-    ip_address = request.headers.get("IP_Address")
-    lang_code = request.LANGUAGE_CODE
-    
-    return Response({
-        "message": "language_changed"
-        , "ip_address": ip_address
-        , "lang_code": lang_code}
-        , status=status.HTTP_200_OK)
+
+@decorators.api_view(["GET", ])
+@decorators.permission_classes([])
+def language_switcher(request: HttpRequest):
+    return Response({"message": "language switched successfully"}, status=status.HTTP_200_OK)
