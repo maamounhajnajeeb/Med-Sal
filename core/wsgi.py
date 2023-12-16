04949f19e18bcbd938267c4a2f2dec02abbe83ce
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-12-16 16:44:12 +0200

    waitress server added

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+# If using WhiteNoise:
+# from whitenoise import WhiteNoise
+# application = WhiteNoise(get_wsgi_application())
+
+# # If using dj-static:
+# from dj_static import Cling
+# application = Cling(get_wsgi_application())
