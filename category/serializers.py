6f3aa7d58bcf9c58059dc3bdec8171f9c251c23e
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-12-23 19:32:51 +0200

    after adding docker and category test finished

--- category/serializers.py
+++ category/serializers.py
@@ -1,48 +1,9 @@
 from rest_framework import serializers
 
 from django.db import connection
-from rest_framework.fields import empty
 
 from .models import Category
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Category
-        fields = "__all__"
-    
-    def __init__(self, instance=None, data=..., **kwargs):
-        language = kwargs.get('language')
-        if language:
-            self.language = kwargs.pop("language")
-        else:
-            self.language = None
-        super().__init__(instance, data, **kwargs)
-    
-    def create(self, validated_data):
-        query = f"insert into category_category (en_name, ar_name) \
-            values ('{validated_data['en_name']}', '{validated_data['ar_name']}')"
-        
-        if validated_data.get("parent"):
-            query = f"insert into category_category (en_name, ar_name, parent_id) \
-                values ('{validated_data['en_name']}', '{validated_data['ar_name']}' \
-                , '{validated_data['parent'].id}')"
-        
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            
-        return Category.objects.all().last()
-    
-    def to_representation(self, instance):
-        origin_repr = super().to_representation(instance)
-        
-        opposite_language = "en" if self.language == "ar" else "ar"
-        origin_repr.pop(f"{opposite_language}_name")
-        origin_repr["name"] = origin_repr.pop(f"{self.language}_name")
-        
-        return origin_repr
-    
 
 class CategoryCUDSerializer(serializers.ModelSerializer):
     
@@ -71,4 +32,41 @@ class CategoryCUDSerializer(serializers.ModelSerializer):
             cursor.execute(query)
             
         return Category.objects.all().last()
+
+
+class CategorySerializer(CategoryCUDSerializer):
     
+    # class Meta:
+    #     model = Category
+    #     fields = "__all__"
+    
+    # def __init__(self, instance=None, data=..., **kwargs):
+    #     language = kwargs.get('language')
+    #     if language:
+    #         self.language = kwargs.pop("language")
+    #     else:
+    #         self.language = None
+    #     super().__init__(instance, data, **kwargs)
+    
+    # def create(self, validated_data):
+    #     query = f"insert into category_category (en_name, ar_name) \
+    #         values ('{validated_data['en_name']}', '{validated_data['ar_name']}')"
+        
+    #     if validated_data.get("parent"):
+    #         query = f"insert into category_category (en_name, ar_name, parent_id) \
+    #             values ('{validated_data['en_name']}', '{validated_data['ar_name']}' \
+    #             , '{validated_data['parent'].id}')"
+        
+    #     with connection.cursor() as cursor:
+    #         cursor.execute(query)
+            
+    #     return Category.objects.all().last()
+    
+    def to_representation(self, instance):
+        origin_repr = super().to_representation(instance)
+        
+        opposite_language = "en" if self.language == "ar" else "ar"
+        origin_repr.pop(f"{opposite_language}_name")
+        origin_repr["name"] = origin_repr.pop(f"{self.language}_name")
+        
+        return origin_repr
--- category/tests.py
+++ category/tests.py
@@ -5,6 +5,8 @@ from django.contrib.auth import get_user_model
 
 from hypothesis.extra.django import TestCase
 
+from .models import Category
+
 
 Users = get_user_model()
 
@@ -158,27 +160,43 @@ class TestUpdateAndDestroyAPI(TestCase):
         
         self.client = APIClient()
     
+    def test_invalid_update_category(self):
+        self.create_category()
+        
+        response = self.client.patch(
+            path="/api/v1/category/update/2/"
+            , headers={"Authorization": f"JWT {self.get_access_token()}"})
+        assert response.status_code == 404
+    
     def test_update_category(self):
         self.create_category()
+        
+        response = self.client.patch(
+            path="/api/v1/category/update/1/"
+            , headers={"Authorization": f"JWT {self.get_access_token()}"})
+        response_data = response.json()
+        
+        assert response_data.get("en_name") == "Doctors"
+        assert response_data.get("ar_name") == "أطباء"
+        assert response.status_code == 200
         
     def test_destroy_category(self):
         self.create_category()
         
+        response = self.client.delete(
+            path="/api/v1/category/destroy/1/"
+            , headers={"Authorization": f"JWT {self.get_access_token()}"})
+        assert response.status_code == 204
+        
     def test_invalid_destroy_category(self):
+        self.create_category()
+        
         response = self.client.delete(
             path="/api/v1/category/destroy/6/"
-            , headers={"Authorization": f"JWT {self.login()}"})
+            , headers={"Authorization": f"JWT {self.get_access_token()}"})
         assert response.status_code == 404
     
-    def create_category(self):
-        access_token = self.login()
-        
-        self.client.post(path="/api/v1/category/create/", data=self.category_data
-            , format="json", headers={"Authorization": f"JWT {access_token}"})
-        
-        return access_token
-    
-    def login(self):
+    def get_access_token(self):
         self.create_group()
         self.create_admin()
         
@@ -189,9 +207,64 @@ class TestUpdateAndDestroyAPI(TestCase):
         
         return response.json()["access"]
     
+    def create_category(self):
+        Category.objects.create(**self.category_data)
+    
     def create_admin(self):
         user = Users.objects.create_user(**self.admin_data)
         return user
     
     def create_group(self):
         Group.objects.create(name=self.group_name)
+
+
+class TestDeniedUser(TestCase):
+    def setUp(self) -> None:
+        self.user_data = {
+            "email": "maamoun.haj.najeeb@gmail.com"
+            , "password": "sv_gtab101enter"
+            , "is_active": True
+            , "user_type": "USER"
+            , "phone": "+963932715313"
+        }
+        
+        self.category_data = {
+            "en_name": "Developers"
+            , "ar_name": "مطورون"
+        }
+        
+        self.group_name = {"name": "USER"}
+        
+        self.client = APIClient()
+    
+    def test_user_update(self):
+        self.create_category()
+        
+        response = self.client.patch(
+            path="/api/v1/category/update/1/", format="json"
+            , headers={"Authorization": f"JWT {self.get_access_token()}"}
+        )
+        
+        assert response.status_code == 403
+        
+    def create_category(self):
+        Category.objects.create(**self.category_data)
+    
+    def get_access_token(self):
+        self.create_group()
+        self.create_user()
+        
+        response = self.client.post(
+            path="/api/v1/users/login/"
+            , data={"email": "maamoun.haj.najeeb@gmail.com", "password": "sv_gtab101enter"}
+            , format="json"
+        )
+        
+        return response.json().get("access")
+    
+    def create_user(self):
+        user = Users.objects.create_user(**self.user_data)
+        return user
+        
+    def create_group(self):
+        Group.objects.create(**self.group_name)
