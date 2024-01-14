f8bbcbbd7df5975aafe99da453cb641c1f8424c3
Maamoun <maamoun.haj.najeeb@gmail.com>
2024-01-14 06:57:23 +0200

    after finishing contact us app

--- contact_us/models.py
+++ contact_us/models.py
@@ -9,8 +9,7 @@ class ContactUs(models.Model):
     mail = models.EmailField(max_length=127, null=False)
     message = models.TextField(null=False)
     read = models.BooleanField(default=False)
-    read_by = models.ForeignKey(Admins, on_delete=models.PROTECT, related_name="contact_us")
+    read_by = models.ForeignKey(Admins, on_delete=models.PROTECT, related_name="contact_us", null=True)
     
     def __str__(self) -> str:
         return f"{self.name}, {self.mail}, {self.read}"
-
--- contact_us/urls.py
+++ contact_us/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     # create for everybody
     path("create/", views.CreateContcatUs.as_view(), name="create_contact_us"),
     
-    # 
+    # list for admins and super admins (is_staff == True)
+    path("list/", views.ListContactUs.as_view(), name="list_contact_us"),
+    
+    # retrieve, update, delete for admins and super admins (is_staff == True)
+    path("<int:pk>/", views.RUDContactUs.as_view(), name="rud_contact_us"),
 ]
